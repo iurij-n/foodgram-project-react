731aa68e6fc670256e7097487d335e5c3ef15d18
Юрий Новиков <iurij.novickov2016@yandex.ru>
2022-06-28 17:39:16 +0300

    Final_1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,16 +4,16 @@ from rest_framework.routers import DefaultRouter
 from .views import (CustomUserViewSet, IngredientViewSet, RecipeViewSet,
                     TagViewSet, UserDetail)
 
-router = DefaultRouter()
+router_v1 = DefaultRouter()
 
-router.register('users', CustomUserViewSet)
-router.register('tags', TagViewSet)
-router.register('recipes', RecipeViewSet, basename='recipes')
-router.register('ingredients', IngredientViewSet)
+router_v1.register('users', CustomUserViewSet)
+router_v1.register('tags', TagViewSet)
+router_v1.register('recipes', RecipeViewSet, basename='recipes')
+router_v1.register('ingredients', IngredientViewSet)
 
 urlpatterns = [
     path('users/<int:pk>/', UserDetail.as_view(), name='user_detail'),
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
     path('', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken')),
 ]
