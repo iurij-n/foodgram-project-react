ab2b8c67a3671eb832fb82b97bea0ed6d772f416
Юрий Новиков <iurij.novickov2016@yandex.ru>
2022-06-28 15:08:03 +0300

    Final_1

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,16 @@ from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from djoser.serializers import SetPasswordSerializer
+from recipe.models import (Favorites, Follow, Ingredient, Recipe,
+                           RecipeIngredient, ShoppingCart, Tag)
 from rest_framework import generics, permissions, viewsets
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
                                    HTTP_400_BAD_REQUEST)
+from users.models import User
+
 from .filters import RecipeFilter
 from .pagination import RecipeListPagination, UserListPagination
 from .permissions import AuthorOrReadOnly
@@ -16,9 +20,6 @@ from .serializers import (AddFavoritesSerializer, AddFollowSerializer,
                           CustomUserCreateSerializer, CustomUserSerializer,
                           IngredientSerializer, RecipeSerializer,
                           TagSerializer)
-from recipe.models import (Favorites, Follow, Ingredient, Recipe,
-                           RecipeIngredient, ShoppingCart, Tag)
-from users.models import User
 
 
 class TagViewSet(viewsets.ReadOnlyModelViewSet):
