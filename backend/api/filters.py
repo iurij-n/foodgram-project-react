8d8e592afb603d50e5470d4436b8d1daea13aa49
Юрий Новиков <iurij.novickov2016@yandex.ru>
2022-06-30 03:04:32 +0300

    Final_3

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,6 +1,6 @@
 from django_filters.rest_framework import FilterSet, filters
-from rest_framework.filters import SearchFilter
 from recipe.models import Recipe
+from rest_framework.filters import SearchFilter
 
 
 class RecipeFilter(FilterSet):
